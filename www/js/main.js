9d5cbdfc38e684cb3944e24e9205d199e5543d78
Tag <tagadvance@gmail.com>
2015-04-25 00:14:26 -0700

    hide layover on mousedown event

--- www/js/main.js
+++ www/js/main.js
@@ -9,22 +9,23 @@ $(function() {
     var $overlay = $("#supernova");
     $("a > img").mouseenter(function(e) {
 	var $image = $(this);
-	
+
 	var clearQueue = true;
 	var jumpToEnd = true;
 	$overlay.stop(clearQueue, jumpToEnd);
 
 	update_position($image, $overlay);
 	$overlay.fadeIn("fast");
+    }).mouseup(function(e) {
+	$overlay.show();
 
-	// reset click-through
-	$overlay.off('click');
-	$overlay.click(function(e) {
-	    $a = $image.parent();
-	    window.location.href = $a.attr('href');
-	});
+	var $image = $(this);
+	$a = $image.parent();
+	window.location.href = $a.attr('href');
     });
-    $overlay.mouseout(function(e) {
+    $overlay.mousedown(function(e) {
+	$overlay.hide();
+    }).mouseout(function(e) {
 	$overlay.fadeOut("fast");
     });
 });
